a8c73c2ae324aced510ff3ad0151efd32ce0da25
Dr. Ken Duna <70976865+KenDuna@users.noreply.github.com>
2020-10-11 15:28:40 -0700

    Update WordCloud Generator.py

--- WordCloud Generator.py
+++ WordCloud Generator.py
@@ -6,20 +6,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-
 from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+## If you are using my Reddit Scraper, it saves the .txt files with utf-8 encoding (this prevents errors). 
+## You will need to convert the .txt to ansi encoding to use this Word Cloud generator.
 
 root = tk.Tk()
 root.withdraw()
-
 file_path = filedialog.askopenfilename()
-
 file = open(file_path, 'rt')
 text = file.read()
 file.close()
-
 
 max_words = input('Enter the maximum number of words you wish to have in the Word Cloud:\n')
 exclusion = input('Enter any words you wish to exclude (separate by a space, not case sensitive):\n')
